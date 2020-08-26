b6dd81f0469c5d94c1084af7306479ea5a6b0d3b
biyachuev <biyachuev@gmail.com>
2020-08-26 21:15:32 +0300

    minor fixes

--- src/Main.kt
+++ src/Main.kt
@@ -1,199 +1,205 @@
 package minesweeper
 
-import kotlin.random.Random
 import java.util.*
+import kotlin.math.abs
+import kotlin.random.Random
 
-const val DEBUG = true
+const val ANSI_RESET = "\u001B[0m"
+const val ANSI_RED = "\u001B[31m"
+const val ANSI_GREEN = "\u001B[32m"
+const val ANSI_BLUE = "\u001B[34m"
 
-// TODO Убрать русские комменты
-// TODO Подумать над try / catch
-// TODO Coords вообще нужен?
-// TODO Вместо DEBUG -> Logger
+const val DEBUG_LVL = 0 // 100 - print uncovered field, 200 - verbose info
 
-class Coords(val x: Int, val y: Int)  // uses for storing of coordinates of cells
+class Cell {
+    var visible: Boolean = false
+    var mine: Boolean = false
+    var minesAround: Int = 0
+    var marked: Boolean = false
+}
 
-object PlayingField {
+object Board {
     private const val size = 9
-    private val field = Array(size) { CharArray(size) { '.' } }
-    private val marks = Array(size) { CharArray(size) { '.' } }
-    private var mines: Int = 0
-    private var markedMines: Int = 0
+    private val field: Array<Array<Cell>> = Array(size) { Array(size) { Cell() } }
+    private var freeCellFirstMove: Boolean = true
     private var minesLeft: Int = 0
-    var steppedOnMine: Boolean = false
-    private var firstMove: Boolean = true
-    private var emptiesNotExplored: Int = 0
 
-    fun init(minesForGame: Int) {
-        mines = minesForGame
-        minesLeft = mines
+    enum class GameStatuses { Continue, SteppedOnMine, ExploredAllEmpties, FoundAllMines, Error }
+    private var gmStatus: GameStatuses = GameStatuses.Continue
+    fun gmStatus(): GameStatuses = gmStatus
+
+    override fun toString(): String {
+        var res = ""
+        res += " │" + CharArray(size) { i -> '1' + i }.joinToString("") + "│" + "\n"
+        res += "—│${"—".repeat(size)}│" + "\n"
+
+        for (i in field.indices) {
+            res += "${i + 1}│"
+            for (el in field[i])
+                res += (when {
+                    DEBUG_LVL >= 100 -> {
+                        when {
+                            el.marked -> ANSI_BLUE + "*" + ANSI_RESET
+                            el.mine -> ANSI_RED + "X" + ANSI_RESET
+                            el.visible && el.minesAround == 0 -> ANSI_BLUE + "/" + ANSI_RESET
+                            el.visible -> ANSI_BLUE + el.minesAround + ANSI_RESET
+                            else -> el.minesAround
+                        }
+                    }
+                    el.mine && gmStatus == GameStatuses.SteppedOnMine -> {
+                        "X" //ANSI_RED + "X" + ANSI_RESET
+                    }
+                    el.marked -> {
+                        "*" //ANSI_BLUE + "*" + ANSI_RESET
+                    }
+                    el.visible && el.minesAround == 0 -> {
+                        "/" //ANSI_GREEN + "/" + ANSI_RESET
+                    }
+                    !el.visible -> {
+                        '.'
+                    }
+                    else -> {
+                        el.minesAround //ANSI_GREEN + el.minesAround + ANSI_RESET
+                    }
+                })
+            res += "│" + "\n"
+        }
+        res += "—│${"—".repeat(size)}│" + "\n"
+        return res
     }
 
-    fun placeMines(minesForGame: Int) {
-        var minesLeftToPlace = minesForGame
+    fun setNumberOfMines(num: Int) {
+        if (num !in 1..90) {
+            println("Number of mines must be between 1..90")
+            gmStatus = GameStatuses.Error
+        } else
+            minesLeft = num
+    }
+
+    private fun outBounds(x: Int, y: Int): Boolean {
+        return x < 0 || y < 0 || x >= size || y >= size
+    }
+
+    private fun calcMinesAround(x: Int, y: Int): Int { // a priori (x,y) is not a mine
+        var temp = 0
+        for (i in -1..1)
+            for (j in -1..1)
+                if (!outBounds(x + i, y + j)) {
+                    if (field[x + i][y + j].mine) temp++
+                }
+        return temp
+    }
+
+    fun executeCmd(y: Int, x: Int, cmd: String) { // y,x
+        when (cmd) {
+            "free" -> freeCell(x, y)
+            "mine" -> markCell(x, y)
+        }
+    }
+
+    private fun placeMines(x_restricted: Int, y_restricted: Int) {
+        var minesLeftToPlace = minesLeft
+        if (DEBUG_LVL == 200) println("-- placeMines")
         do {
             val x = Random.nextInt(size)
             val y = Random.nextInt(size)
-            if (field[x][y] != 'X') {
-                field[x][y] = 'X'
+            if (DEBUG_LVL == 200) println("-- ($x,$y)")
+            if (!field[x][y].mine && !(abs(x - x_restricted) <= 1 && abs(y - y_restricted) <= 1)) {
+                field[x][y].mine = true
                 minesLeftToPlace--
-                // calculate neighbor cells
-                for (i in maxOf(0, x - 1)..minOf(size - 1, x + 1))
-                    for (j in maxOf(0, y - 1)..minOf(size - 1, y + 1))
-                        when (field[i][j]) {
-                            '.' -> field[i][j] = '1'
-                            in '1'..'9' -> field[i][j]++
+
+                for (i in -1..1)
+                    for (j in -1..1)
+                        if (!outBounds(x + i, y + j)) {
+                            field[x + i][y + j].minesAround++
                         }
             }
         } while (minesLeftToPlace > 0)
-
-        // calculate empties
-        emptiesNotExplored = 0
-        for (i in 0 until size)
-            for (j in 0 until size)
-                if (field[i][j] == '.') println("i = $i j = $j empties = ${++emptiesNotExplored}")
-        println("EMPTIES: $emptiesNotExplored")
+        if (DEBUG_LVL == 200) println(Board)
     }
 
-    fun setCell(y: Int, x: Int, cmd: String) {
-        when {
-            cmd == "free" && firstMove -> { // the first cell explored with the free command cannot be a mine; it should always be empty.
-                while (field[x][y] == 'X') {
-                    field[x][y] = '.'
-                    placeMines(1)
-                    if (DEBUG) println("переставляем мины")
-                }
-                marks[x][y] = '/'
-                firstMove = false
-                if (DEBUG) { println("Вот поле после возможной перестановки"); printField(showMines = true, isDebug = true) }
-                recalculate(x, y)
-            }
-            cmd == "free" && field[x][y] == 'X' -> {
-                steppedOnMine = true
-            }
-            cmd == "free" -> { // marks = {. or 1..9} && field <> 'X'
-                if (marks[x][y] == '*') markedMines-- // free/explore cell previously marked as mine
-                marks[x][y] = '/'
-                recalculate(x, y)
-            }
-            cmd == "mine" && marks[x][y] == '*' -> { // delete own mark, marks = {*, . or 1..9} && field <> 'X'
-                marks[x][y] = '.' // initial state
-                markedMines--
-                if (field[x][y] == 'X') minesLeft++
-            }
-            cmd == "mine" && marks[x][y] in '1'..'8' -> { // error move
-                println("There is a number here!")
-            }
-            cmd == "mine" && field[x][y] == '.' -> { // marked empty cell
-                marks[x][y] = '*'
-                markedMines++
-            }
-            cmd == "mine" && field[x][y] == 'X' -> { // marked a bomb
-                marks[x][y] = '*'
-                markedMines++
-                minesLeft--
-            }
-        }
-    }
-
-    private fun recalculate(x_init: Int, y_init: Int) {
-        // x,y - is empty cell. find new empty surrounding cells and display them at marks array
-
-        var x = x_init
-        var y = y_init
-
-        val a = mutableListOf<Coords>() // storing of founded empty cells
-        do {
-            if (!a.isNullOrEmpty()) {
-                x = a[a.lastIndex].x  // переименовать в a[0].x
-                y = a[a.lastIndex].y
-                a.removeAt(a.lastIndex)
-//                print("Остались: ")
-//                a.forEach { e -> print("(${e.x + 1},${e.y + 1}) ") }
-//                println()
-            }
-
-//            println("Взяли клетку x=${x+1},y=${y+1}")
-//            println("значение empties: $emptiesNotExplored")
-            printField(showMines = true, isDebug = true)
-
-            for (i in maxOf(0, x - 1)..minOf(size - 1, x + 1))
-                for (j in maxOf(0, y - 1)..minOf(size - 1, y + 1))
-                    if (field[i][j] == '.') {
-                        print("((${i + 1},${j + 1})=${field[i][j]} ")
-                        field[i][j] = '/'
-                        marks[i][j] = '/'
-                        a.add(Coords(i, j))
-                        emptiesNotExplored--
-                        println("($i,$j) emptiesNotExplored: $emptiesNotExplored")
+    private fun reveal(x: Int, y: Int) {
+        field[x][y].visible = true
+        for (i in -1..1)
+            for (j in -1..1)
+                if (!outBounds(x + i, y + j)) {
+                    if (DEBUG_LVL == 200) println("-- (${x + i},${y + j}) = ${calcMinesAround(x + i, y + j)}")
+                    if (calcMinesAround(x + i, y + j) == 0 && !(i == 0 && j == 0) && !field[x + i][y + j].visible) {
+                        reveal(x + i, y + j)
+                    } else if (!field[x + i][y + j].mine) {
+                        field[x + i][y + j].visible = true
+                        field[x + i][y + j].marked = false // especially for 8th test -> cell was marked as mine, but it is a neighbour of empty cell
                     }
-            println()
-//            print("после того как прошли цикл, нашли следующие пустые клетки и поставили на них А: ")
-//            a.forEach { e -> print("(${e.x + 1},${e.y + 1}) ") }
-//            println()
-
-        } while (!a.isNullOrEmpty())
+                }
     }
 
-
-    fun gameIsOver(): Boolean {
-        return (minesLeft == 0 && markedMines == mines) || steppedOnMine || emptiesNotExplored == 0
-    }
-
-    fun printField(showMines: Boolean, isDebug: Boolean) {
-        if (isDebug) println("DEBUG MODE") else println("GAME MODE")
-        //println(" │123456789│")
-        print(" │")
-        print(CharArray(size) { i -> '1' + i })
-        println("│")
-        println("—│${"—".repeat(size)}│")
-        for (i in 0 until size) {
-            print("${i + 1}│")
-            for (j in 0 until size)
-                print(when {
-                    marks[i][j] != '.' -> marks[i][j]
-                    field[i][j] == 'X' && !showMines -> '.'
-                    field[i][j] == 'X' && showMines -> 'X'
-                    isDebug -> field[i][j]
-                    else -> '.'
-                })
-            println("│")
+    private fun freeCell(x: Int, y: Int) {
+        if (DEBUG_LVL == 200) println("-- freeCell")
+        when {
+            freeCellFirstMove -> {
+                freeCellFirstMove = false
+                placeMines(x, y)
+                freeCell(x, y)
+            }
+            field[x][y].mine -> {
+                gmStatus = GameStatuses.SteppedOnMine
+                return
+            }
+            !field[x][y].marked && !field[x][y].visible && calcMinesAround(x,y) > 0 -> {
+                field[x][y].visible = true // point a cell with a number
+            }
+            !field[x][y].marked && !field[x][y].visible -> {
+                reveal(x,y)
+            }
         }
-        println("—│${"—".repeat(size)}│")
+        gmStatus = GameStatuses.ExploredAllEmpties    // if explored all empties -> game ends. Lets check it
+        for (i in field.indices)
+            for (el in field[i])
+                if (!el.visible && !el.mine) { // if there is a cell which is not visible(explored) and not a mine - than game continues
+                    gmStatus = GameStatuses.Continue
+                    break
+                }
     }
+
+    private fun markCell(x: Int, y: Int) {
+        when {
+            field[x][y].marked -> {
+                field[x][y].marked = false
+            }
+            field[x][y].visible -> {
+                println(ANSI_RED + "Already explored!" + ANSI_RESET)
+                return
+            }
+            else -> {
+                field[x][y].marked = true
+            }
+        }
+
+        gmStatus = GameStatuses.FoundAllMines     // found all mines -> game ends. Lets check it
+        for (i in field.indices)
+            for (el in field[i])
+                if (el.marked xor el.mine) {    // if there is a cell which is not visible(explored) and not a mine -> than game continues
+                    gmStatus = GameStatuses.Continue
+                    break
+                }
+    }
+
 }
 
 fun main() {
     val scanner = Scanner(System.`in`)
     print("How many mines do you want on the field? > ")
-    val minesForGame = scanner.nextInt()
-    if (!(minesForGame in 1..80)) {
-        println("Number of mines must be between 1..80")
-        return
-    }
-
-    PlayingField.init(minesForGame)
-    PlayingField.placeMines(minesForGame)
-
-//    PlayingField.printField(showMines = true, isDebug = true)
-//    PlayingField.printField(showMines = true, isDebug = false)
+    Board.setNumberOfMines(scanner.nextInt())
+    if (Board.gmStatus() != Board.GameStatuses.Error) print(Board) else return
 
     do {
-        PlayingField.printField(showMines = true, isDebug = true)
-        PlayingField.printField(showMines = false, isDebug = false)
         print("Set/unset mine marks or claim a cell as free: > ")
-        PlayingField.setCell(scanner.nextInt() - 1, scanner.nextInt() - 1, scanner.next())
-    } while (!PlayingField.gameIsOver())
+        Board.executeCmd(scanner.nextInt() - 1, scanner.nextInt() - 1, scanner.next()) // e.g. "1 1 free" -> (0, 0, "free")
+        print(Board)
+    } while (Board.gmStatus() == Board.GameStatuses.Continue)
 
-    PlayingField.printField(showMines = true, isDebug = true)
-    PlayingField.printField(showMines = true, isDebug = false)
-    if (PlayingField.steppedOnMine) {
-        println("You stepped on a mine and failed!")
-    } else println("Congratulations! You found all the mines!")
+    println(when (Board.gmStatus()) {
+        Board.GameStatuses.SteppedOnMine -> ANSI_RED + "You stepped on a mine and failed!" + ANSI_RESET
+        else -> ANSI_GREEN + "Congratulations! You found all the mines!" + ANSI_RESET
+    })
 }
-
-// Legend
-//  . as unexplored cells
-//  / as explored free cells without mines around it
-//  Numbers from 1 to 8 as explored free cells with 1 to 8 mines around them, respectively
-//  X as mines
-//  * as unexplored marked cells
